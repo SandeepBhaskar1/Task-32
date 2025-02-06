b48e893e2a4751cb1412a3e4615d731ca9b93897
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-02-06 19:58:49 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://task-32-frontend.vercel.app',
     credentials: true
 }));
 
